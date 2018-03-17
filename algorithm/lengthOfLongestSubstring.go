a68f83ace0fb2cf5a72969cf1d7be625934e4e8f
cherry chan <cherrycmd@gmail.com>
2018-03-17 13:19:46 +0800

    length of longest

---
+++ algorithm/lengthOfLongestSubstring.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+func lengthOfLongestSubstring(s string) int {
+	slength := len(s)
+	ans := 0
+	for i := 0; i < slength; i++ {
+		for j := i + 1; j <= slength; j++ {
+			if judgeUnique(s, i, j) {
+				ans = max(ans, j-i)
+			}
+		}
+	}
+	return ans
+}
+
+func max(x int, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+func judgeUnique(s string, start int, end int) bool {
+	chars := make(map[string]int)
+	for i := start; i < end; i++ {
+		_, ok := chars[string(s[i])]
+		if ok {
+			return false
+		} else {
+			chars[string(s[i])] = 1
+		}
+	}
+	return true
+}
+
+func main() {
+	ans := lengthOfLongestSubstring("bbbbb")
+	fmt.Println(ans)
+	fmt.Println("vim-go")
+}
