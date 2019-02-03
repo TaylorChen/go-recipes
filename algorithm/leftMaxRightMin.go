2d93c03cda64a74c6b7bea561cae9af6f03c9443
Cherry <cherrycmd@gmail.com>
2019-02-04 07:51:10 +0800

    add

---
+++ algorithm/leftMaxRightMin.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func findThePivotElement(arr []int, len int) {
+	rightMin := make([]int, len)
+	r_min := arr[len-1]
+	for i := len - 2; i >= 0; i-- {
+		if arr[i] < r_min {
+			r_min = arr[i]
+		}
+		rightMin[i] = r_min
+	}
+
+	l_max := arr[0]
+	for i := 1; i < len-1; i++ {
+		if arr[i] > l_max {
+			l_max = arr[i]
+			if arr[i] < rightMin[i+1] {
+				fmt.Println(arr[i])
+			}
+		}
+	}
+
+}
+
+func main() {
+	st := []int{1, 8, 6, 9, 10, 15, 12, 20}
+	findThePivotElement(st, len(st))
+}
