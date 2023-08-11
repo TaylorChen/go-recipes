6fe247b11504f2e2128da49babefe3787f58aec0
chenrui <chenrui@aceplaying.com>
2023-08-11 10:20:21 +0800

    update

--- algorithm/rightMaxArr.go
+++ algorithm/rightMaxArr.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	. "./collections"
 	"fmt"
+	"go-recipes/algorithm/collections"
 )
 
 func rightMaxArr(arr []int, len int) []int {
 	res := make([]int, len)
-	stack := Stack{}
+	stack := collections.Stack{}
 	stack.Push(0)
 	for i := 1; i < len; i++ {
 		for stack.Len() != 0 && arr[i] > arr[(stack.Peek()).(int)] {
